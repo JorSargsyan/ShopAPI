25235ed5bc7d101cded19bd6b1e7873a31f74b8a
Jora Sargsyan <jorsargsyan@gmail.com>
2023-10-25 13:03:14 +0400

    feat: added role permission table insertion when creating roles

--- application/src/role/create.rs
+++ application/src/role/create.rs
@@ -1,9 +1,10 @@
 use diesel::prelude::*;
-use domain::roles::{NewRole, Role};
+use domain::roles::{NewRole, Role, RolePermission};
 use infrastructure::establish_connection;
 use rocket::{response::status::Created, serde::json::Json};
 
 pub fn create_role(form_data: Json<NewRole>) -> Created<String> {
+    use domain::schema::role_permissions;
     use domain::schema::roles;
     let new_role = form_data.into_inner();
 
@@ -16,7 +17,31 @@ pub fn create_role(form_data: Json<NewRole>) -> Created<String> {
         .values(&role)
         .get_result::<Role>(&mut establish_connection())
     {
-        Ok(_) => Created::new(""),
+        Ok(role) => {
+            let mut role_perms: Vec<RolePermission> = vec![];
+
+            for item in new_role.permission_ids.into_iter() {
+                let role_perm = RolePermission {
+                    id: None,
+                    role_id: role.id.unwrap(),
+                    permission_id: item,
+                };
+
+                role_perms.push(role_perm)
+            }
+
+            match diesel::insert_into(role_permissions::table)
+                .values(&role_perms)
+                .execute(&mut establish_connection())
+            {
+                Ok(_) => Created::new(""),
+                Err(err) => match err {
+                    _ => {
+                        panic!("Database error - {}", err);
+                    }
+                },
+            }
+        }
         Err(err) => match err {
             _ => {
                 panic!("Database error - {}", err);
--- domain/src/roles/mod.rs
+++ domain/src/roles/mod.rs
@@ -1,11 +1,17 @@
 #![allow(unused)]
 #![allow(clippy::all)]
 
-use diesel::prelude::{Identifiable, Insertable, Queryable};
+use diesel::{
+    prelude::{Associations, Identifiable, Insertable, Queryable},
+    query_builder::QueryId,
+    Selectable,
+};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use crate::schema::{role_permissions, roles};
+
+use crate::permissions::Permission;
 
 #[derive(Queryable, Debug, Identifiable, Serialize, Deserialize, Insertable)]
 #[diesel(primary_key(id))]
@@ -23,9 +29,14 @@ pub struct NewRole {
     pub title: String,
 }
 
-#[derive(Queryable, Debug)]
+#[derive(Queryable, Debug, Associations, Identifiable, Selectable, Insertable)]
+#[diesel(belongs_to(Role))]
+#[diesel(belongs_to(Permission))]
+#[diesel(table_name= role_permissions)]
+#[diesel(primary_key(id))]
 pub struct RolePermission {
-    pub id: i32,
-    pub role_id: Option<i32>,
-    pub permission_id: Option<i32>,
+    #[diesel(deserialize_as = i32)]
+    pub id: Option<i32>,
+    pub permission_id: i32,
+    pub role_id: i32,
 }
